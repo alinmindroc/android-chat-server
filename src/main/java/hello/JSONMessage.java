fb5bf8b07c3602c8b58a10722cb7ba3b7daac970
Alin Mindroc <mindroc.alin@gmail.com>
2016-04-18 00:05:56 +0300

    first commit, simple get and post requests

---
+++ src/main/java/hello/JSONMessage.java
@@ -0,0 +1,89 @@
+package hello;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Date;
+
+public class JSONMessage {
+    private String content;
+    private Date date;
+    private String senderId;
+    private String receiverId;
+    private String senderName;
+    private String receiverName;
+
+    @JsonCreator
+    public JSONMessage() {
+        super();
+    }
+
+    public JSONMessage(String content, Date date, String senderId, String receiverId, String senderName, String receiverName) {
+        this.content = content;
+        this.date = date;
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+        this.senderName = senderName;
+        this.receiverName = receiverName;
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public String getReceiverName() {
+        return receiverName;
+    }
+
+    public void setReceiverName(String receiverName) {
+        this.receiverName = receiverName;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+
+    public String getReceiverId() {
+        return receiverId;
+    }
+
+    public void setReceiverId(String receiverId) {
+        this.receiverId = receiverId;
+    }
+
+    @Override
+    public String toString() {
+        return "JSONMessage{" +
+                "content='" + content + '\'' +
+                ", date=" + date +
+                ", senderId='" + senderId + '\'' +
+                ", receiverId='" + receiverId + '\'' +
+                ", senderName='" + senderName + '\'' +
+                ", receiverName='" + receiverName + '\'' +
+                '}';
+    }
+}
